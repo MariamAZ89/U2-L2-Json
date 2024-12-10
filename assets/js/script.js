fac72142d189cc7b0271a0da360a33aadc6d4cdc
Mariam Azfar Azami <azami.maria89@gmail.com>
2024-12-10 16:46:30 +0100

    in corso

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const nameIput = document.getElementById("name");
+const surnameInput = document.getElementById("surname");
+const ageInput = documet.getElementById("age");
+const deletInput = document.getElementById("save");
+const saveInput = document.getElementById("save");
+const 
