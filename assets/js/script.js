e3c82c6a53b228ea28e6f1da34b82101ff3d5c6a
Mariam Azfar Azami <azami.maria89@gmail.com>
2024-12-10 17:06:14 +0100

    inCorso

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,27 @@ const surnameInput = document.getElementById("surname");
 const ageInput = documet.getElementById("age");
 const deletInput = document.getElementById("save");
 const saveInput = document.getElementById("save");
-const 
+const conterLabel = document.getElementById("counter");
+const textLabel = documeent.getElementById("text");
+
+let count = 0;
+
+const add = function () {
+  let StringaUser = `Name: ${nameInput.value} \n Surname: ${surnameInput.value} \n Age: ${ageInput.value}`;
+  let user = localStorage.setItem("User, StringaUser");
+  if (user) {
+    load();
+  }
+  nameInput.value = "";
+  surnameInput.value = "";
+  ageInput.value = "";
+};
+const load = () => {
+  textLabel.innerText = localStorage.getItem("User");
+};
+saveButton.addEventListner("click", () => {
+  if (nameInput.value && surnameInput.value && ageInput.value) {
+    add();
+  }
+  load();
+});
